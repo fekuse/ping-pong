59aae61f772eadc14249549e4158c0b18075df4b
fekuse <127673981+fekuse@users.noreply.github.com>
2023-03-26 18:29:01 +0400

    add game sprite and player

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 from pygame import *
 
+class Player(GameSprite):
+   #метод для управления спрайтом стрелками клавиатуры
+   def update_1(self):
+        keys = key.get_pressed()
+        if keys[K_W] and self.rect.y > 5:
+            self.rect.y -= self.speed
+ #метод "выстрел" (используем место игрока, чтобы создать там пулю)
+    def update_r(self):
+    keys = key.get_pressed() 
+    if keys[K_UP] and self.rect.y > 5:
+        self.rect.y -= self.speed
+
+class GameSprite (sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (wight, height)) #BMеCT
+        self.speed= player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+        def reset(self):
+            window.blit(self.image, (self.rect.x, self.rect.y))
+
 #создаём окошко
 win_width = 700
 win_height = 500
