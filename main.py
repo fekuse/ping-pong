980936b1442fbc7cf8aa3b4b295d038b37bd021f
fekuse <127673981+fekuse@users.noreply.github.com>
2023-04-02 17:54:17 +0400

    e2673t66326rdf6238gdcfd67w3rfd22726dt2eee
    препод сказал, что можно написать любую абракадабру

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 from pygame import *
 
-class Player(GameSprite):
-   #метод для управления спрайтом стрелками клавиатуры
-    def update_1(self):
-        keys = key.get_pressed()
-        if keys[K_W] and self.rect.y > 5:
-            self.rect.y -= self.speed
- #метод "выстрел" (используем место игрока, чтобы создать там пулю)
-    def update_r(self):
-    keys = key.get_pressed() 
-    if keys[K_UP] and self.rect.y > 5:
-        self.rect.y -= self.speed
-
 class GameSprite (sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
         super().__init__()
@@ -21,22 +9,58 @@ class GameSprite (sprite.Sprite):
         self.rect.x = player_x
         self.rect.y = player_y
 
-        def reset(self):
-            window.blit(self.image, (self.rect.x, self.rect.y))
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+   #метод для управления спрайтом стрелками клавиатуры
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 480:
+            self.rect.y += self.speed
+ #метод "выстрел" (используем место игрока, чтобы создать там пулю)
+    def update_r(self):
+        keys = key.get_pressed() 
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 480:
+            self.rect.y += self.speed
+
+
 
 #создаём окошко
-win_width = 700
+win_width = 600
 win_height = 500
 back = (200,255,251)
 display.set_caption("Shooter")
 window = display.set_mode((win_width, win_height))
 window.fill(back)
 #создаём спрайты
-#основной цикл игры:
-run = True #флаг сбрасывается кнопкой закрытия окна
-while run:
+#основной цик
+racket1 = Player("racket.png", 30, 200, 4, 50, 150)
+racket2 = Player("racket.png", 520, 200, 4, 50, 150)
+ball = GameSprite("ball.png", 200,200,4,50,50)
+game = True #флаг сбрасывается кнопкой закрытия окна
+
+clock = time.Clock()
+
+while game:
    #событие нажатия на кнопку Закрыть
     for e in event.get():
         if e.type == QUIT:
-            run = False
+            game = False
+
+    
+    window.fill(back)
+    racket1.update_l()
+    racket2.update_r()
+    
+    racket1.reset()
+    racket2.reset()
+    ball.reset()
+
+
     display.update()
+    clock.tick(60)
